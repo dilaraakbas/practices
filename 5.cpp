d8a425c306a78d41686faedbe720c1bb33477355
dilaraakbas <167521467+dilaraakbas@users.noreply.github.com>
2024-11-28 14:11:43 +0300

    Project5

---
+++ 5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	for(int i=0;i<100;i+=10){
+		for(int j=2;j<=10;j+=2)
+		printf("%d\t" , (i+j));
+		printf("\n");
+	}
+	return 0;
+}
